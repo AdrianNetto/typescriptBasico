fea12c729f28888d8f9e0e1990aaa495a8980c16
Adrian Netto <adrian.netto73@gmail.com>
2024-01-02 15:26:13 -0300

    challenge

---
+++ 2_fundamentos/desafio/index.js
@@ -0,0 +1,6 @@
+var i = 1;
+console.log(i);
+console.log(typeof i);
+var iString = i.toString();
+console.log(iString);
+console.log(typeof iString);
---
+++ 2_fundamentos/desafio/index.ts
@@ -0,0 +1,9 @@
+let i: number = 1;
+
+console.log(i);
+console.log(typeof i);
+
+let iString = i.toString();
+
+console.log(iString);
+console.log(typeof iString);
