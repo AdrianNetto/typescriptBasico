5b9dadd56b7f77355f5498a98910d8ef4e7f3e8e
Adrian Netto <adrian.netto73@gmail.com>
2024-01-03 15:50:26 -0300

    name added

--- 6_object_types/index.ts
+++ 6_object_types/index.ts
@@ -1,29 +1,61 @@
 // tipo de objeto para função com interface
 
 interface Product {
-  name: string
-  price: number
-  isAvailable: boolean
+  name: string;
+  price: number;
+  isAvailable: boolean;
 }
 
 function showProductDetails(product: Product) {
-  console.log(`O nome do produto é ${product.name} e seu preço é R$${product.price}`)
-  if(product.isAvailable) {
-    console.log("O produto está disponível")
+  console.log(
+    `O nome do produto é ${product.name} e seu preço é R$${product.price}`
+  );
+  if (product.isAvailable) {
+    console.log("O produto está disponível");
   }
 }
 
-const shirt:Product = {
+const shirt: Product = {
   name: "Camisa",
   price: 59.99,
-  isAvailable: true
-}
+  isAvailable: true,
+};
 
-showProductDetails(shirt)
+showProductDetails(shirt);
 
-const pant:Product = {
+const pant: Product = {
   name: "Calça",
   price: 29.99,
-  isAvailable: false
+  isAvailable: false,
+};
+
+// interface com parametro opcional
+
+interface User {
+  nome: string;
+  email: string;
+  role?: string;
 }
 
+function showUserDetails(user: User) {
+  if (user.role) {
+    console.log(
+      `O user tem um email de ${user.email} e um cargo de ${user.role}`
+    );
+  }
+  console.log(`O user tem um email de ${user.email}`);
+}
+
+const usuario = {
+  nome: "Adrian",
+  email: "user.test@gmail.com",
+};
+
+const usuarioRole = {
+  nome: "João",
+  email: "user.test@gmail.com",
+  role: "adm",
+};
+
+showUserDetails(usuario);
+showUserDetails(usuarioRole);
