193ff9818107cafaeae4ef2ba2e249022daeb3f2
Adrian Netto <adrian.netto73@gmail.com>
2024-01-19 10:52:56 -0300

    router parameters and +

--- 13_express/src/app.ts
+++ 13_express/src/app.ts
@@ -40,6 +40,55 @@ app.all("/api/product/check", (req, res) => {
   }
 });
 
+// interfaces do express
+
+app.get("/api/interfaces", (req: Request, res: Response) => {
+  return res.send("Utilizando as interfaces");
+});
+
+// enviando JSON
+
+app.get("/api/json", (req: Request, res: Response) => {
+  return res.json({
+    name: "T-Shirt",
+    price: 30.0,
+    color: "Black",
+    sizes: ["P", "M", "G", "GG", "XG"],
+  });
+});
+
+// router parameters
+
+app.get("/api/product/:id", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const id = req.params.id;
+
+  if (id === "1") {
+    const product = {
+      id: 1,
+      brand: "Chevrolet",
+      model: "Chevette",
+      engine: "1.6",
+      tractionType: "RWD",
+    };
+
+    return res.json(product);
+  } else {
+    return res.send("Produto não encontrado");
+  }
+});
+
+// rotas complexas
+
+app.get("/api/product/:id/reviews/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const productid = req.params.id;
+  const reviewId = req.params.reviewId;
+
+  return res.send(`Acessando a review ${reviewId} para o produto ${productid}`);
+});
 
 app.listen(port, () => {
   console.log(`🚀 Server running on ${url} 🚀`);
